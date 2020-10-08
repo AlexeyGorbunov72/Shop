189414fc57e125a6752a96d416f531624b22debb
AnyDodic <korri.13@inbox.ru>
2020-10-08 03:39:00 +0300

    add lib

--- Shop/main.swift
+++ Shop/main.swift
@@ -6,18 +6,35 @@
 //
 
 import Foundation
-func govno(){
-    let shop: Shop? = Shop()
-    var shops: [Shop]? = []
-    for _ in 0...100{
-        shops!.append(Shop())
+import ShopOOP
+
+func setRandomPrices(products: [Product]){
+    for priduct in products{
+        priduct.setPrice(price: Double.random(in: 2...1000))
     }
-
-    print(ShopsNetwork.getShops().count)
-    ShopsNetwork.deleteShop(shop: shop!)
-    print(ShopsNetwork.getShops().count)
-    ShopsNetwork.deleteShop(shops: shops!)
-    print(ShopsNetwork.getShops().count)
 }
-govno()
+
+let products = [
+    Product(title: "хуй резиновый", price: 1488),
+    Product(title: "вибратор", price: 228),
+    Product(title: "навоз", price: 13),
+    Product(title: "раб чорный", price: 27),
+    Product(title: "пивас", price: 42),
+    Product(title: "Алексей Папков Раб для ебли", price: Double.infinity)
+]
+var shops: [Shop] = []
+for i in 0...10{
+    shops.append(Shop())
+    for _ in 0...Int.random(in: 3...100){
+        setRandomPrices(products: products)
+        shops[i].putProduct(product: products.randomElement()!)
+    }
+    print("shop uid: \(shops[i].getUID())")
+    shops[i].printProducts()
+}
+let productToTest = products.randomElement()!
+let productToTest1 = products.randomElement()!
+print(productToTest.title, productToTest1.title)
+print(try ShopsNetwork.findChiperConsignment(consignment: [(product: productToTest, count: 1), (product: productToTest1, count: 1)]))
 
+shops[2].getAllProductsBy(price: 1000).forEach{ print("\($0.product.title): \($0.count)")}
